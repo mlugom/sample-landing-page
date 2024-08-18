1e603369a39de0a579a300803e904db2e9600cce
Manuel <me.lm2207@gmail.com>
2024-08-18 16:50:33 -0500

    refactor: wrap blue background color in custom css class
    Also, it was added a custom class for the blue border

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ function HeroText() {
         This website has some subtext that goes here under the main title. It's
         a smaller font and the color is lower contrast.
       </p>
-      <button className="rounded-md bg-[#3882F6] px-7 py-2 font-semibold">Sign up</button>
+      <button className="rounded-md blue-background px-7 py-2 font-semibold">Sign up</button>
     </div>
   );
 }
