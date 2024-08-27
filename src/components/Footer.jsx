d4bb29409c99b50ea6f1460f268ac92653175991
Manuel <me.lm2207@gmail.com>
2024-08-27 12:31:14 -0500

    feat: add anchor tag to 'The Odin Project' in header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,11 @@ export default function Footer() {
   return (
     <footer className="dark-background text-center p-7 text-white">
       <p className="font-thin text-small">
-        Copyright &copy; The Odin Project 2021
+        Copyright &copy;{" "}
+        <a href="https://www.theodinproject.com" className="hover:underline">
+          The Odin Project 2021
+        </a>
       </p>
     </footer>
-  )
+  );
 }
